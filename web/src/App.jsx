917a0d51b01afc0fda64d455bf5f7c4ce7822279
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-09-01 10:46:32 +0530

    hero section started

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React,{Suspense} from 'react'
+import "./Assets/App.css"
+const Hero = React.lazy(() => import('./Components/Hero'))
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hero />
+      </Suspense>
+    </div>
   )
 }
 
---
+++ web/src/Components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../Assets/Hero.css"
+function Hero() {
+  return (
+    <section
+    className='container center hero-section-container'>
+      <h1>Hello</h1>
+      <h2>I'm <span>Aswin</span></h2>
+      <p>A full stack developer and an engineer based in India</p>
+      <ul>
+        <li>
+          <button>
+            Say Hi
+          </button>
+        </li>
+        <li>
+          <button>
+            View Resume
+          </button>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Hero
