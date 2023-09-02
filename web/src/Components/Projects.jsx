8afa6b8c70880d6df0e90eae1f1b53477c6b0d09
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-09-02 22:57:45 +0530

    some projects added

--- web/src/Components/Projects.jsx
+++ web/src/Components/Projects.jsx
@@ -27,6 +27,18 @@ function Projects() {
             link:'https://github.com/aswnss-m/secondBooks'
 
         }}/>
+        <Card3d props = {{
+          title: 'Paithrika Homes',
+          status: 'online',
+          tech: 'figma',
+          link : 'https://www.figma.com/proto/e16RYgLCQ0aL0iqoAfQh6y/Works?type=design&node-id=346-931&t=HZja74mALKdltpTj-1&scaling=min-zoom&page-id=345%3A694&starting-point-node-id=346%3A931&show-proto-sidebar=1&mode=design'
+        }}/>
+        <Card3d props = {{
+          title: 'IEEE SCT SB Redesign',
+          status: 'online',
+          tech: 'figma',
+          link : "https://www.figma.com/proto/e16RYgLCQ0aL0iqoAfQh6y/Works?type=design&node-id=346-1870&t=xJkeS5mW6OFq1zhF-0&scaling=min-zoom&page-id=345%3A694&starting-point-node-id=346%3A1870&show-proto-sidebar=1"
+        }}/>
         </div>
     </section>
   )
